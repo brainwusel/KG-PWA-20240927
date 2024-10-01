cd35f9f3b65c3a053826826718e96f36c626a08d
brainwusel <aig09@uni-koeln.de>
2024-10-01 17:33:41 +0200

    2024-10-01-17-33

--- script.js
+++ script.js
@@ -5,39 +5,21 @@ var outputHeight = document.getElementById("outputHeight")
 var containerHeight = document.getElementById("containerHeight")
 var bodyHeight =document.getElementById("bodyHeight")
 
-
-
-
-/*inputText.addEventListener('input', () => { */
-
-function displayText() {    
-    output.style.fontSize = "1px"
-    var fontGroesse = 1
-    /*outputHeight.textContent = output.clientHeight
-    containerHeight.textContent = container.clientHeight
-    bodyHeight.textContent = screen.availHeight*/
-
-    while(container.clientHeight < screen.availHeight/1.5) {
-        fontGroesse = fontGroesse + 1
-        output.style.fontSize = `${fontGroesse-1}px`;
-        output.textContent = inputText.value;
-    } 
-
-    inputText.value=" "
-};
-
-/*
-function resize_to_fit() {
-    var output = document.getElementById("output");
-    var container = document.getElementById("container");
-    let fontSize = window.getComputedStyle(output).fontSize;
-    output.style.fontSize = parseFloat(fontSize) - 1 + "px"
-
-    if (output.clientHeight >= container.clientHeight) {
-      resize_to_fit();
+function displayText() {  
+   
+    if (inputText.value) {
+        output.style.fontSize = "1px";
+        var fontGroesse = 1;
+        while((container.clientHeight < screen.availHeight/1) & (container.clientWidth < screen.availWidth)) {
+            fontGroesse = fontGroesse + 1;
+            output.style.fontSize = `${fontGroesse+5}px`;
+            output.textContent = inputText.value;
+        } 
+        inputText.value="";
+    } else {
+        inputText.value="";
     }
 }
-*/
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
