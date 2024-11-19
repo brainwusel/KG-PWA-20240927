1ba1edb117ef3730b022b3ab0b3020d3ae7626dd
brainwusel <aig09@uni-koeln.de>
2024-11-19 18:11:40 +0100

    2024-11-19-18-11

--- script.js
+++ script.js
@@ -229,7 +229,7 @@ function displayText() {
                     outputTitel.style.overflowWrap = "normal";
                     outputTitel.textContent = m.titel;
                 } while ((outputTitel.clientHeight < containerTitel.clientHeight) & (outputTitel.clientWidth <= containerTitel.clientWidth))
-                fontGroesse = fontGroesse + 5;
+                fontGroesse = fontGroesse + 2;
                 outputTitel.style.fontSize = `${fontGroesse}px`;
             }
             if (m.tonart !== " ") {
@@ -241,7 +241,7 @@ function displayText() {
                     outputTonart.style.overflowWrap = "normal";
                     outputTonart.textContent = m.tonart;
                 } while ((outputTonart.clientHeight < containerTonart.clientHeight) & (outputTonart.clientWidth <= containerTonart.clientWidth))
-                fontGroesse = fontGroesse + 5;
+                fontGroesse = fontGroesse + 2;
                 outputTonart.style.fontSize = `${fontGroesse}px`;
             }
             break;
